b7be443a25451fcf96c00766620463db20918dad
Mr-Diaz430 <Mr.diaz430@gmail.com>
2026-01-07 19:54:07 -0300

    enrutamiento y settings base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { AboutUs } from './pages/about-us/about-us';
+import { Products } from './pages/products/products';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'nosotros', component: AboutUs},
+    {path: 'productos', component: Products},
+    {path: 'contactos', component: Contact},
+    {path: '**', redirectTo: ''}
+];
+
+
+
+
+
+
+
